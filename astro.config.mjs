339a2e4d7ae0b72ad95ddb47da029f73b7ac4f90
Willie Pai <PaisWillie@gmail.com>
2024-06-07 15:45:00 -0400

    feat: replace template defaults

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://ses.eng.mcmaster.ca/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
