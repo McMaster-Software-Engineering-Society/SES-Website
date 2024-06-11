e535865b65f9d027dd3f29a86a445e337ad9cc4b
Willie Pai <PaisWillie@gmail.com>
2024-06-11 13:58:03 -0400

    feat: add PWA support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,25 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
+import AstroPWA from "@vite-pwa/astro";
 import icon from "astro-icon";
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ses.eng.mcmaster.ca/",
-  integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
+  integrations: [tailwind(), mdx(), sitemap(), icon(), react(), AstroPWA(
+    {
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: 'auto',
+      workbox: {
+        navigateFallback: "/404",
+      },
+    }
+  )],
 });
