cd64703749fc9fafe453edf4c61c60fb12208cc0
Willie Pai <Willie.Pai@soti.net>
2024-06-12 14:05:15 -0400

    feat: add social media constants

---
+++ src/data/social-media.ts
@@ -0,0 +1,20 @@
+import { IconType } from '@react-icons/all-files/lib/index.mjs';
+import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram';
+import { FaFacebook } from '@react-icons/all-files/fa/FaFacebook';
+
+export const socialMedia: {
+  platform: string;
+  icon: IconType;
+  url: string;
+}[] = [
+  {
+    platform: 'Instagram',
+    icon: FaInstagram,
+    url: 'https://www.instagram.com/mcmaster_ses/',
+  },
+  {
+    platform: 'Facebook',
+    icon: FaFacebook,
+    url: 'https://www.facebook.com/McMasterSEC/',
+    },
+]
