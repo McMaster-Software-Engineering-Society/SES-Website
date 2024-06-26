bb032cfc88fa54da6a3a582b681d46cf608b0471
Willie Pai <Willie.Pai@soti.net>
2024-06-26 16:22:09 -0400

    fix: linting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,11 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection } from "astro:content";
 
 // 2. Define your collection(s)
 const eventCollection = defineCollection({
   schema: z.object({
     title: z.string(),
-    dateTime: z.string().transform(str => new Date(str)),
+    dateTime: z.string().transform((str) => new Date(str)),
     location: z.string(),
     image: z.object({
       src: z.string(),
@@ -18,5 +18,5 @@ const eventCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'events': eventCollection,
+  events: eventCollection,
 };
