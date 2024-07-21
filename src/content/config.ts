29a6d6ac228259a52dab4c8cee2d079f811dce41
Willie Pai <PaisWillie@gmail.com>
2024-07-20 23:27:27 -0400

    feat: init blog layout structure

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,6 +2,22 @@
 import { z, defineCollection } from "astro:content";
 
 // 2. Define your collection(s)
+const blogCollection = defineCollection({
+  schema: z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    snippet: z.string(),
+    image: z.object({
+      src: z.string(),
+      alt: z.string(),
+    }),
+    publishDate: z.string().transform(str => new Date(str)),
+    author: z.string(),
+    category: z.string(),
+    tags: z.array(z.string()),
+  }),
+});
+
 const eventCollection = defineCollection({
   schema: z.object({
     title: z.string(),
@@ -47,6 +63,7 @@ const internshipListCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
+  blogs: blogCollection,
   events: eventCollection,
   mcmasterHackathons: hackathonCollection,
   externalHackathons: hackathonCollection,
