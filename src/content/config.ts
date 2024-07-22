7b9822646060f2d858334dbf6b2291fa903f17a2
Willie Pai <Willie.Pai@soti.net>
2024-07-22 10:52:57 -0400

    feat: add dynamic blog list

--- src/content/config.ts
+++ src/content/config.ts
@@ -63,7 +63,7 @@ const internshipListCollection = defineCollection({
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  blogs: blogCollection,
+  blogPosts: blogCollection,
   events: eventCollection,
   mcmasterHackathons: hackathonCollection,
   externalHackathons: hackathonCollection,
