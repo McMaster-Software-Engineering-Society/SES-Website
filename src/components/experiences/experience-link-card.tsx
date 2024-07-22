d6fc475a07f17bf1c9155a9b0d642767718dca5c
Willie Pai <Willie.Pai@soti.net>
2024-07-22 16:42:38 -0400

    feat: complete experiences card

--- src/components/experiences/experience-link-card.tsx
+++ src/components/experiences/experience-link-card.tsx
@@ -6,11 +6,23 @@ type ExperienceLinkCardProps = {
   link: ExperienceLink;
 };
 
-export default function ExperienceLinkCard({ link }: ExperienceLinkCardProps) {
+export default function ExperienceLinkCard({
+  link,
+}: Readonly<ExperienceLinkCardProps>) {
   // TODO: https://nextui.pro/components/application/cards
   return (
-    <Card>
-      <CardBody>{link.title}</CardBody>
+    <Card className="border-1.5 border-gray-700 p-1">
+      <CardBody className="flex flex-row gap-x-4">
+        <Card className="border-1 border-gray-800">
+          <CardBody>
+            <link.icon size={20} className="text-slate-400" />
+          </CardBody>
+        </Card>
+        <div className="flex flex-col">
+          <span className="font-medium">{link.title}</span>
+          <span className="text-slate-500">{link.description}</span>
+        </div>
+      </CardBody>
     </Card>
   );
 }
