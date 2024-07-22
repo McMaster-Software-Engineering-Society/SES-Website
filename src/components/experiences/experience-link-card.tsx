03db7e45bb1dd6244fad4be35859252525a4eccc
Willie Pai <Willie.Pai@soti.net>
2024-07-22 17:13:35 -0400

    feat: add link to experience

--- src/components/experiences/experience-link-card.tsx
+++ src/components/experiences/experience-link-card.tsx
@@ -9,20 +9,21 @@ type ExperienceLinkCardProps = {
 export default function ExperienceLinkCard({
   link,
 }: Readonly<ExperienceLinkCardProps>) {
-  // TODO: https://nextui.pro/components/application/cards
   return (
-    <Card className="border-1.5 border-gray-700 p-1">
-      <CardBody className="flex flex-row gap-x-4">
-        <Card className="border-1 border-gray-800">
-          <CardBody>
-            <link.icon size={20} className="text-slate-400" />
-          </CardBody>
-        </Card>
-        <div className="flex flex-col">
-          <span className="font-medium">{link.title}</span>
-          <span className="text-slate-500">{link.description}</span>
-        </div>
-      </CardBody>
-    </Card>
+    <a href={link.href}>
+      <Card className="w-full border-1.5 border-gray-700 hover:border-purple-600 p-1 hover:-translate-y-[2px] active:translate-y-0  hover:bg-zinc-800 group">
+        <CardBody className="flex flex-row gap-x-4">
+          <Card className="border-1 border-gray-800 group-hover:border-purple-800 group-hover:bg-zinc-800">
+            <CardBody>
+              <link.icon size={20} className="text-slate-400" />
+            </CardBody>
+          </Card>
+          <div className="flex flex-col">
+            <span className="font-medium">{link.title}</span>
+            <span className="text-slate-500">{link.description}</span>
+          </div>
+        </CardBody>
+      </Card>
+    </a>
   );
 }
