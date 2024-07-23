be15147d97b8e8950689e92527a1a0c88946eaf7
Willie Pai <PaisWillie@gmail.com>
2024-07-23 10:16:42 -0400

    fix: experience card icon

--- src/components/experiences/experience-link-card.tsx
+++ src/components/experiences/experience-link-card.tsx
@@ -12,8 +12,8 @@ export default function ExperienceLinkCard({
   return (
     <a href={link.href}>
       <Card className="w-full border-1.5 border-gray-700 hover:border-purple-600 p-1 hover:-translate-y-[2px] active:translate-y-0  hover:bg-zinc-800 group">
-        <CardBody className="flex flex-row gap-x-4">
-          <Card className="border-1 border-gray-800 group-hover:border-purple-800 group-hover:bg-zinc-800">
+        <CardBody className="flex flex-row gap-x-4 items-start">
+          <Card className="border-1 border-gray-800 group-hover:border-purple-800 group-hover:bg-zinc-800 min-w-min max-h-min">
             <CardBody>
               <link.icon size={20} className="text-slate-400" />
             </CardBody>
