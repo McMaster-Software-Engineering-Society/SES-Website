84459f21b14a7a8651396e6e3c60d814ba0bdf61
Willie Pai <PaisWillie@gmail.com>
2024-09-20 18:49:01 -0400

    feat: change server port to 80

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,4 +10,7 @@ import react from "@astrojs/react";
 export default defineConfig({
   site: "https://ses.eng.mcmaster.ca/",
   integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
+  server: {
+    port: 80,
+  },
 });
