00ca0bb5ea19167d1363b685dd7fcaa649f4aea9
Willie Pai <PaisWillie@gmail.com>
2024-09-24 13:09:07 -0400

    fix: linting

--- src/data/sponsorship.ts
+++ src/data/sponsorship.ts
@@ -1,1 +1,2 @@
-export const sponsorshipPackageURL = "/contact/sponsorship/SES Sponsorship Package 2024 - 2025.pdf";
+export const sponsorshipPackageURL =
+  "/contact/sponsorship/SES Sponsorship Package 2024 - 2025.pdf";
